0e823b2a2960a49c82c622bc4de8844f65cc66bc
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2024-02-29 21:11:00 -0500

    Added sxhkd and bspwm

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -2,23 +2,31 @@
 
 DOTFILES_DIR=$HOME/.dotfiles
 
-# List of dotfiles
-declare -a FILES_TO_SYMLINK=(
-  'vim/.vimrc'
-  'bash/.bashrc'
-  'bash/.bash_profile'
+# List of dotfiles with their target directories
+declare -A FILES_TO_SYMLINK=(
+  [vim/.vimrc]="$HOME/"
+  [bash/.bashrc]="$HOME/"
+  [bash/.bash_profile]="$HOME/"
+  [bspwm/bspwmrc]="$HOME/.config/bspwm/"
+  [sxhkd/sxhkdrc]="$HOME/.config/sxhkd/"
+  [sxhkd/sxhkdrc_office]="$HOME/.config/sxhkd/"
   # add other dotfiles here
 )
 
-# Create symlinks in the home directory pointing to files in the .dotfiles directory
-for i in "${FILES_TO_SYMLINK[@]}"; do
+# Create symlinks for the files in their specified target directories
+for i in "${!FILES_TO_SYMLINK[@]}"; do
   sourceFile="$DOTFILES_DIR/$i"
-  targetFile="$HOME/$(basename $i)"
+  targetDir="${FILES_TO_SYMLINK[$i]}"
+  targetFile="${targetDir}$(basename $i)"
+
+  # Ensure the target directory exists
+  mkdir -p "$targetDir"
 
   if [ -e "$targetFile" ]; then
     if [ -L "$targetFile" ]; then
       # It's already a symlink (no action required)
       echo "Symlink exists for $i, skipping."
+      continue # Skip to the next file
     else
       # It's a real file, back it up
       echo "Backing up $targetFile."
