8447ad9dd296b96e0b9babe6762a1f841ea6c3c0
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-03 14:45:43 -0500

    Added neovim plugins to nix

--- nix-home-manager/modules/editors/neovim/nvim.nix
+++ nix-home-manager/modules/editors/neovim/nvim.nix
@@ -21,5 +21,114 @@ in
 
 	'';
 
+	  plugins = with pkgs.vimPlugins; [
+      plenary
+      nvim-web-devicons
+      nvim-treesitter
+      nvim-treesitter-context
+      nvim-treesitter-textobjects
+      telescope-nvim
+      vim-tmux-navigator
+      harpoon
+      ripgrep
+      leap-nvim
+      copilot-vim
+      vim-css-color
+      mason-nvim
+      mason-lspconfig
+      nvim-lspconfig
+      SimpylFold
+      vimtex
+      vim-tex-fold
+      vimwiki
+      calendar-vim
+      vim-smoothie
+      limelight-vim
+      lightline-vim
+      vimspector
+      fzf-vim
+      fzf
+      startup-nvim
+      telescope-ultisnips-nvim
+      ultisnips
+      vim-snippets
+      # add other plugins here
+    ];
+
+	#plugins = with pkgs.vimPlugins; [
+    #  plenary-nvim
+
+    #  # Treesitter
+    #  nvim-web-devicons
+    #  (nvim-treesitter.withPlugins (p: [
+    #  	tree-sitter-textobjects
+    #  ])))
+
+    #  telescope-nvim
+    #  vim-tmux-navigator
+    #  harpoon
+    #  ripgrep
+    #  leap-nvim
+    #  copilot-vim
+
+    #  'ap/vim-css-color',
+
+    #  'williamboman/mason.nvim',
+    #  'williamboman/mason-lspconfig.nvim',
+    #  'neovim/nvim-lspconfig',
+
+    #  # Python
+    #  'tmhedberg/SimpylFold',
+
+    #  # Latex
+    #  'lervag/vimtex',
+    #  'matze/vim-tex-fold',
+
+    #   # Vimwiki
+    #  {
+    #    "vimwiki/vimwiki",
+    #    init = function()
+    #        vim.g.vimwiki_list = {
+    #            {
+    #                path = '~/Documents/wiki/Research',
+    #                syntax = 'default',
+    #    			listsyms = '✗○◐●✓',
+    #                ext = '.wiki',
+    #            },
+    #        }
+    #    end,
+    #  },
+
+
+    #  # Vim Plugins
+    #  'mattn/calendar-vim',
+    #  'psliwka/vim-smoothie',
+    #  'junegunn/limelight.vim',
+    #  'itchyny/lightline.vim',
+    #  'puremourning/vimspector',
+    #  'https://github.com/kshenoy/vim-signature',
+    #  {
+    #    'junegunn/fzf',
+    #    build = function()
+    #      vim.fn['fzf#install']()
+    #    end
+    #   },
+
+    #  {
+    #    'startup-nvim/startup.nvim',
+    #    dependencies = {'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim'},
+    #    config = function()
+    #      require'startup'.setup()
+    #    end
+    #  },
+    #
+    #  # UltiSnips
+    #  'jdujava/telescope-ultisnips.nvim',
+    #  # I've been trying to make my vim ultisnips work with nvim. Another snippets are in the folder: 
+    #  #/home/vasilii/.local/share/nvim/site/pack/packer/start/vim-snippets/UltiSnips/tex.snippets:148
+    #  'SirVer/ultisnips',
+    #  'honza/vim-snippets',
+    #
+	#]
   };
 }
