1af4715c6300b70e89896f34480dab54867057b8
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-03 23:06:49 -0500

    Trying to make ultisnips work

--- nvim/lua/keymaps/ultisnips.lua
+++ nvim/lua/keymaps/ultisnips.lua
@@ -1,5 +1,9 @@
 vim.g.UltiSnipsExpandTrigger = "<tab>"
 vim.g.UltiSnipsJumpForwardTrigger = "<c-f>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<c-d>"
-vim.g.UltiSnipsSnippetDir = "~/.dotfiles/nvim/UltiSnips"
-vim.g.UltiSnipsSnippetDirectories = {"~/.dotfiles/nvim/UltiSnips"}
+local home = os.getenv("HOME")
+vim.g.UltiSnipsSnippetDir = { '/home/vasilii/.dotfiles/nvim/UltiSnips' }
+vim.g.UltiSnipsSnippetDirectories = { '/home/vasilii/.dotfiles/nvim/UltiSnips' }
+--vim.g.UltiSnipsSnippetDir = "~/.dotfiles/nvim/UltiSnips"
+--vim.g.UltiSnipsSnippetDirectories = {"~/.dotfiles/nvim/UltiSnips"}
+
