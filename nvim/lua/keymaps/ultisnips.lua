e702c2dd239dfe3675f539f011519d25a23465e4
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2024-03-03 22:02:05 -0500

    Fixed ultisnips file paths

--- nvim/lua/keymaps/ultisnips.lua
+++ nvim/lua/keymaps/ultisnips.lua
@@ -1,5 +1,5 @@
 vim.g.UltiSnipsExpandTrigger = "<tab>"
 vim.g.UltiSnipsJumpForwardTrigger = "<c-f>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<c-d>"
-vim.g.UltiSnipsSnippetDir = "~/.config/nvim/UltiSnips"
-vim.g.UltiSnipsSnippetDirectories = {"~/.config/nvim/UltiSnips"}
+vim.g.UltiSnipsSnippetDir = "~/.dotfiles/nvim/UltiSnips"
+vim.g.UltiSnipsSnippetDirectories = {"~/.dotfiles/nvim/UltiSnips"}
