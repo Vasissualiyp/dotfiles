efcd26784d17a16a8dc98bb3ee745beb8e3a0246
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-03 23:13:15 -0500

    Added correct UltiSnips, along with their assignments

--- nvim/lua/keymaps/ultisnips.lua
+++ nvim/lua/keymaps/ultisnips.lua
@@ -2,8 +2,8 @@ vim.g.UltiSnipsExpandTrigger = "<tab>"
 vim.g.UltiSnipsJumpForwardTrigger = "<c-f>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<c-d>"
 local home = os.getenv("HOME")
-vim.g.UltiSnipsSnippetDir = { '/home/vasilii/.dotfiles/nvim/UltiSnips' }
-vim.g.UltiSnipsSnippetDirectories = { '/home/vasilii/.dotfiles/nvim/UltiSnips' }
+vim.g.UltiSnipsSnippetDir =         { '~/.dotfiles/nvim/UltiSnips' }
+vim.g.UltiSnipsSnippetDirectories = { '~/.dotfiles/nvim/UltiSnips' }
 --vim.g.UltiSnipsSnippetDir = "~/.dotfiles/nvim/UltiSnips"
 --vim.g.UltiSnipsSnippetDirectories = {"~/.dotfiles/nvim/UltiSnips"}
 
