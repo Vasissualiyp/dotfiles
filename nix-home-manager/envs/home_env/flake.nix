2649d41fb71ab952bb3f6b084d47f227e45f5c84
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-06 18:11:41 -0500

    Changed the python envs flake

--- nix-home-manager/envs/home_env/flake.nix
+++ nix-home-manager/envs/home_env/flake.nix
@@ -1,25 +1,33 @@
 {
-  description = "Home python environments";
+  description = "A very basic flake";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
 
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux = {
-      general = nixpkgs.legacyPackages.x86_64-linux.python3.withPackages (ps: with ps; [
-        numpy
-        pandas
-      ]);
 
-      vim = nixpkgs.legacyPackages.x86_64-linux.python3.withPackages (ps: with ps; [
-        pynvim
-        pandas
-      ]);
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-      shellgpt = nixpkgs.legacyPackages.x86_64-linux.python3.withPackages (ps: with ps; [
-        scipy
-        matplotlib
-      ]);
-    };
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
   };
-}
+  #outputs = { self, nixpkgs }: {
+  #  packages.x86_64-linux = {
+  #    general = nixpkgs.legacyPackages.x86_64-linux.python3.withPackages (ps: with ps; [
+  #      numpy
+  #      pandas
+  #    ]);
 
+  #    vim = nixpkgs.legacyPackages.x86_64-linux.python3.withPackages (ps: with ps; [
+  #      pynvim
+  #      pandas
+  #    ]);
+
+  #    shellgpt = nixpkgs.legacyPackages.x86_64-linux.python3.withPackages (ps: with ps; [
+  #      scipy
+  #      matplotlib
+  #    ]);
+  #  };
+  #};
+}
