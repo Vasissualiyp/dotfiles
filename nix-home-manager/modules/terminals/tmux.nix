5a6f57a8f8c627c0d4280fecfd3f7a9fc1769be8
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-05 22:11:50 -0500

    Can correctly compile the tmux in home-manager

--- nix-home-manager/modules/terminals/tmux.nix
+++ nix-home-manager/modules/terminals/tmux.nix
@@ -1,44 +1,50 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
-with lib;
-
-let
-  cfg = config.programs.tmux;
-in
 {
   programs.tmux = {
+    enable = true;
+	terminal = "tmux-256color";
     plugins = with pkgs;
 	[
-      tmuxPlugins.catpuccin
-	  tmuxPlugins.tmux-sensible
+	  {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+        set -g @catppuccin_flavour 'mocha'
+		set -g @catppuccin_status_modules_left "session"
+        set -g @catppuccin_status_modules_right "directory date_time"
+        set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+        set -g @catppuccin_window_default_text "#W"
+        set -g @catppuccin_status_left_separator  ""
+        set -g @catppuccin_status_right_separator ""
+        set -g @catppuccin_window_left_separator ""
+        set -g @catppuccin_window_right_separator ""
+        #set -g @catppuccin_window_middle_separator "█ "
+        set -g @catppuccin_window_middle_separator ""
+        set -g @catppuccin_window_number_position "left"
+        set -g @catppuccin_window_default_fill "number"
+        set -g @catppuccin_status_fill "icon"
+        set -g @catppuccin_status_connect_separator "no"
+        set -g @catppuccin_status_right_separator_inverse "no"
+        set -g @catppuccin_session_text "#S"
+        
+        set -g @catppuccin_directory_color "#502B49"
+        set -g @catppuccin_directory_icon "#{thm_fg}#[fg=default]"
+        set -g @catppuccin_session_color "#37447a"
+        set -g @catppuccin_session_icon "#{thm_fg}#[fg=default]"
+        set -g @catppuccin_foreground_color "#7a8497"
+        set -g @catppuccin_window_default_fill "number"
+		'';
+	  }
+	  {
+	    plugin = tmuxPlugins.continuum;
+        extraConfig = "set -g @continuum-restore 'on'";
+      }
+	  tmuxPlugins.sensible
+	  tmuxPlugins.yank
+	  tmuxPlugins.resurrect
 	  tmuxPlugins.vim-tmux-navigator
-	  tmuxPlugins.tmux-yank
-	  tmuxPlugins.tmux-resurrect
-	  tmuxPlugins.tmux-continuum
 	];
-  };
-
-  options.programs.tmux = {
-    enable = mkEnableOption "Tmux";
-
-    extraConfig = mkOption {
-      type = types.lines;
-      default = "";
-      description = "Additional tmux configuration.";
-    };
-
-    plugins = mkOption {
-      type = types.listOf types.attrs;
-      default = [];
-      example = literalExample "with pkgs.tmuxPlugins; [ tmux-sensible ]";
-      description = "List of tmux plugins to install.";
-    };
-  };
-
-  config = mkIf cfg.enable {
-    home.packages = [ pkgs.tmux ];
-
-    xdg.configFile."tmux/tmux.conf".text = ''
+	extraConfig = ''
       # Your original tmux configuration goes here
       set-option -sa terminal-overrides ",xterm*:Tc"
       set-option -ga terminal-overrides ",xterm-256color:Tc"
@@ -74,52 +80,10 @@ in
       bind -n M-H previous-window
       bind -n M-L next-window
       
-      set -g @catppuccin_flavour 'mocha'
-      
-      set -g @plugin 'tmux-plugins/tpm'
-      set -g @plugin 'tmux-plugins/tmux-sensible'
-      set -g @plugin 'christoomey/vim-tmux-navigator'
-      #set -g @plugin 'dreamsofcode-io/catppuccin-tmux'
-      set -g @plugin 'catppuccin/tmux'
-      set -g @plugin 'tmux-plugins/tmux-yank'
-      set -g @plugin 'tmux-plugins/tmux-resurrect'
-      set -g @plugin 'tmux-plugins/tmux-continuum'
-      
-      #set -g @plugin 'pschmitt/tmux-ssh-split' # ssh-split plugin
-      #set-option -g @ssh-split-h-key "|" #"|"
-      #set-option -g @ssh-split-v-key "S" #"S"
-      #set-option -g @ssh-split-w-key "c" #"C"
-      #set-option -g @ssh-split-no-shell "false"
-      
       set -g status-position top
-      #set -g @catppuccin_window_left_separator "█"
       set -g @sessionx-zoxide-mode 'on'
-      set -g @catppuccin_status_modules_left "session"
-      set -g @catppuccin_status_modules_right "directory date_time"
-      set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
-      set -g @catppuccin_window_default_text "#W"
-      set -g @catppuccin_status_left_separator  ""
-      set -g @catppuccin_status_right_separator ""
-      set -g @catppuccin_window_left_separator ""
-      set -g @catppuccin_window_right_separator ""
-      #set -g @catppuccin_window_middle_separator "█ "
-      set -g @catppuccin_window_middle_separator ""
-      set -g @catppuccin_window_number_position "left"
-      set -g @catppuccin_window_default_fill "number"
-      set -g @catppuccin_status_fill "icon"
-      set -g @catppuccin_status_connect_separator "no"
-      set -g @catppuccin_status_right_separator_inverse "no"
-      set -g @catppuccin_session_text "#S"
-      
-      set -g @catppuccin_directory_color "#502B49"
-      set -g @catppuccin_directory_icon "#{thm_fg}#[fg=default]"
-      set -g @catppuccin_session_color "#37447a"
-      set -g @catppuccin_session_icon "#{thm_fg}#[fg=default]"
-      set -g @catppuccin_foreground_color "#7a8497"
-      set -g @catppuccin_window_default_fill "number"
       
       
-      set -g @continuum-restore 'on'
       
       bind-key b send-prefix # Requirement for remote sessions
       
@@ -137,20 +101,7 @@ in
       
       bind '"' split-window -v -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
-
-      # TPM (Tmux Plugin Manager) initialization
-      run '~/.tmux/plugins/tpm/tpm'
-
-      # TPM plugins
-      ${concatStringsSep "\n" (map (plugin: "set -g @plugin '${plugin}'") cfg.plugins)}
-      ${cfg.extraConfig}
-    '';
-
-    home.activation.tmuxPlugins = dag.entryAfter [ "writeBoundary" ] ''
-      if [ ! -d $HOME/.tmux/plugins/tpm ]; then
-        git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-      fi
-    '';
+	'';
   };
 }
 
