dbc341499cc37475d7b8414c34b75e329bb09741
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-25 00:43:32 -0400

    Working home manager monitor automatic chooser

--- home-manager-nix-update.sh
+++ home-manager-nix-update.sh
@@ -2,5 +2,12 @@
 
 nix_dir="$HOME/nix"
 nix_system_dir="$nix_dir/home-manager/"
+hostname=$(uname -n)
+numberOfMonitors=$(xrandr | grep " connected" | wc -l)
 
-home-manager switch --flake "$nix_system_dir"
+if [[ "$hostname" == "Vas-Office-Nix" ]]; then
+  hostname="$hostname-${numberOfMonitors}mon"
+fi
+
+export NUMBER_OF_MONITORS="$monitor_number"
+home-manager switch --flake "$nix_system_dir.#$hostname"
