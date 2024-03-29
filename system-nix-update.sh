ff16fd06270bd6b46931bd8c4fc6a1690bbd1266
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-29 17:48:04 -0400

    Now hopefully system update will be asking for a system name only if the flag -n is passed

--- system-nix-update.sh
+++ system-nix-update.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
-# Prompt for hostname
-read -p "Enter hostname (press enter to use current hostname): " input_hostname
+
+# Initialize a variable to track if the -n flag is passed
+n_flag=0
+
+# Parse command-line options
+while getopts "n" opt; do
+  case ${opt} in
+    n )
+      n_flag=1
+      ;;
+    \? ) echo "Usage: cmd [-n]"
+      exit 1
+      ;;
+  esac
+done
+
+# Shift off the options and optional --
+shift "$((OPTIND-1))"
+
+# Assume remaining arguments could be the hostname
+input_hostname=$1
+
+# Use the provided code block only if the -n flag is passed
+if [[ $n_flag -eq 1 ]]; then
+  read -p "Enter hostname (press enter to use current hostname): " input_hostname
+fi
 
 # Use current hostname if input is empty
 if [ -z "$input_hostname" ]; then
@@ -8,6 +32,7 @@ if [ -z "$input_hostname" ]; then
 else
     hostname=$input_hostname
 fi
+
 
 nix_dir="$HOME/nix"
 nix_system_dir="$nix_dir/system"
