59b82f6eaba0db4ce421588fc13d69c38a9a6abb
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-07-11 19:48:32 -0400

    Added syncthing

--- home-manager-nix-update.sh
+++ home-manager-nix-update.sh
@@ -5,6 +5,11 @@ nix_system_dir="$nix_dir/home-manager/"
 hostname=$(uname -n)
 numberOfMonitors=$(xrandr | grep " connected" | wc -l)
 
+# Force at least a single monitor - useful for updating config over ssh
+if [ "$numberOfMonitors" -lt 1 ]; then
+    numberOfMonitors=1
+fi
+
 if [[ "$hostname" == "Vas-Office-Nix" ]]; then
   hostname="$hostname-${numberOfMonitors}mon"
 fi
