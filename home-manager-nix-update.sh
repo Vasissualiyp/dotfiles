a978179c0dcfd7a640881a0dff622e5b35b9f605
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-11-25 08:41:17 -0500

    Added VITURE setup to Vas-Office-Nix

--- home-manager-nix-update.sh
+++ home-manager-nix-update.sh
@@ -4,7 +4,7 @@ nix_dir="$HOME/nix"
 nix_system_dir="$nix_dir/home-manager/"
 hostname=$(uname -n)
 #numberOfMonitors=$(xrandr | grep " connected" | wc -l) 
-numberOfMonitors=$(hyprctl monitors | grep "Monitor" | wc -l)
+numberOfMonitors=$(hyprctl monitors | grep -c "Monitor")
 
 # Force at least a single monitor - useful for updating config over ssh
 if [ "$numberOfMonitors" -lt 1 ]; then
@@ -14,8 +14,15 @@ fi
 if [[ "$hostname" == "Vas-Office-Nix" ]]; then
   hostname="$hostname-${numberOfMonitors}mon"
 fi
+VITURE_NUM=$(hyprctl monitors | grep model | grep -c VITURE)
 
-echo "Hostname: " $hostname
+if [[ "$hostname" == "Vas-Office-Nix-2mon" ]]; then
+  if [[ "$VITURE_NUM" == 1 ]]; then
+    hostname="$hostname-VITURE"
+  fi
+fi
+
+echo "Hostname: " "$hostname"
 
 export NUMBER_OF_MONITORS="$monitor_number"
 home-manager switch --flake "$nix_system_dir.#$hostname"
