dc8dc16c4bfa39fd944d3b755c67cecfa52e5d00
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-11-26 16:11:22 -0500

    Added more complex logic on possibility of having VITURE glasses

--- home-manager-nix-update.sh
+++ home-manager-nix-update.sh
@@ -5,18 +5,30 @@ nix_system_dir="$nix_dir/home-manager/"
 hostname=$(uname -n)
 #numberOfMonitors=$(xrandr | grep " connected" | wc -l) 
 numberOfMonitors=$(hyprctl monitors | grep -c "Monitor")
+VITURE_NUM=$(hyprctl monitors | grep model | grep -c VITURE)
 
 # Force at least a single monitor - useful for updating config over ssh
 if [ "$numberOfMonitors" -lt 1 ]; then
     numberOfMonitors=1
 fi
 
-if [[ "$hostname" == "Vas-Office-Nix" ]]; then
+if [[ "$hostname" == "Vas-Office-Nix" ]] || [[ "$hostname" == "Vas-HP-Nix" ]] ; then
+  MULTIMON=1
+else
+  MULTIMON=0
+fi
+
+if [[ "$MULTIMON" == 1 ]]; then
   hostname="$hostname-${numberOfMonitors}mon"
 fi
-VITURE_NUM=$(hyprctl monitors | grep model | grep -c VITURE)
 
-if [[ "$hostname" == "Vas-Office-Nix-2mon" ]]; then
+if [[ "$hostname" == "Vas-Office-Nix-2mon" ]] || [[ "$hostname" == "Vas-HP-Nix-2mon" ]] ; then
+  VITURE_POSSIBLE=1
+else
+  VITURE_POSSIBLE=0
+fi
+
+if [[ "$VITURE_POSSIBLE" == 1 ]]; then
   if [[ "$VITURE_NUM" == 1 ]]; then
     hostname="$hostname-VITURE"
   fi
