0e6482a0cdcb3aabd63c2aef15d2609ed78e82c9
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2025-01-28 17:01:28 -0500

    Added pwd snippet

---
+++ nvim/LuaSnip/all/scripts.lua
@@ -0,0 +1,16 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
+ls.config.setup({enable_autosnippets = true})
+local as = ls.extend_decorator.apply(s, { snippetType = "autosnippet" })
+
+return {
+
+  -- Get current path
+  s({trig = "pwd"}, {
+    f(function()
+      return vim.fn.getcwd()
+    end),
+  }),
+}
